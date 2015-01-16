fb45f79e32d4c5fde1f0fa2d0deb0e2a0b32733d
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2015-01-16 20:17:09 +0900

    make installの形を変更

--- Makefile
+++ Makefile
@@ -1,8 +1,5 @@
 install-vim:
-	git clone https://github.com/gmarik/Vundle.vim.git ~/dotfiles/.vim/bundle/Vundle.vim
-	git clone https://github.com/tomasr/molokai.git ~/dotfiles/.vim/colors/molokai.git
-	ln -s ${HOME}/dotfiles/.vim ${HOME}/.vim
-	ln -s ${HOME}/dotfiles/.vimrc ${HOME}/.vimrc
+	@sh install.sh
 
 install-atom:
 	ln -s ${HOME}/dotfiles/.atom ${HOME}/.atom
---
+++ install.sh
@@ -0,0 +1,12 @@
+
+if [ -f $HOME/dotfiles/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/gmarik/Vundle.vim.git $HOME/dotfiles/.vim/bundle/Vundle.vim
+fi
+
+if [ -f $HOME/dotfiles/.vim/colors/molokai.git ]; then
+    git clone https://github.com/tomasr/molokai.git $HOME/dotfiles/.vim/colors/molokai.git
+fi
+
+ln -sf ${HOME}/dotfiles/.vim ${HOME}/.vim
+ln -sf ${HOME}/dotfiles/.vimrc ${HOME}/.vimrc
+
